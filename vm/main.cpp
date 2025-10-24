bc5dd5625810918dc505bd1de7f337ff662fc73c
meian <geguji60@163.com>
2025-10-24 18:06:17 +0800

    Add files via upload
    稍微规范一下

---
+++ vm/main.cpp
@@ -0,0 +1,31 @@
+#include "version.h"
+#include "error/error.h"
+#include "op/Loader.h"
+#include "vm.h"
+
+#include <chrono>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        std::cout << "\n    Usage: " << argv[0] << " [options] <file> [args]\n\n";
+        format_out_version();
+        return 0;
+    }
+    
+    Loader loader(argv[1]);
+    auto code = loader.load();
+    const auto entry = loader.entry();
+
+    VirtualMachine vm(code);
+    auto start = std::chrono::high_resolution_clock::now();
+    try {
+        vm.run(entry);
+    } catch (const LmError &e) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto dur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Finish in " << dur_ms << std::endl;
+}
